8c3a45e499a9409aaf5f0d27e808bb9d661a3848
kenrick95 <kenrick95@gmail.com>
2015-03-07 21:42:20 +0800

    Refactor
    try to use websocket instead of p2p's webrtc (which also uses ws)
    not yet working

--- dot-star-webrtc.js
+++ dot-star-webrtc.js
@@ -1,194 +1,132 @@
-function log(who, msg, time) {
-	
-	if (time == null)
-	{
-		var date = new Date();
-		time = date.toISOString();
-	}
-	readable_date = new Date(time);
-	readable_time = readable_date.toUTCString();
-	
-	if (who == null) {
-		who = "Information";
-	}
-	format_msg = ">> ";
-	format_msg += who;
-	format_msg += " &lt;";
-	format_msg += readable_time;
-	format_msg += "&gt; ";
-	format_msg += msg;
-	format_msg += "<br />";
-	
-	msg_before = who;
-	$("#log").html($("#log").html() + format_msg);
-	console.log(who + " <" + time + "> " + msg);
-	scroll_bottom();
-}
-
+/*jslint sloppy: true, plusplus:true, browser: true, unparam:true, vars:true, continue:true */
+/*global $, jQuery, WebSocket, game, update, x ,y */
+"use strict";
 // Initialization
-var brokerSession = null;
-var brokerUrl = 'https://mdsw.ch:8080';
-var hosting = true;
-var options;
+var ws = new WebSocket("ws://nodejs-kenrick95.rhcloud.com:8000");
 var name;
 var msg_before = "";
 var connected = 0;
+var hosting = true;
 
-// If "client", connect to "host"
-if(window.location.search) {
-	var params = window.location.search.substring(1).split('&');
-	for(var i = 0; i < params.length; ++ i) {
-		if(params[i].match('^webrtc-session')) {
-			brokerSession = params[i].split('=')[1];
-			hosting = false;
-		} else if(params[i].match('^webrtc-broker')) {
-			brokerUrl = params[i].split('=')[1];
-		}
-	}
+function scroll_bottom() {
+    // Scroll to bottom of #log
+    var log_area = document.getElementById("log");
+    log_area.scrollTop = log_area.scrollHeight;
 }
 
-console.log('broker', brokerUrl);
-var peer = new Peer(brokerUrl, {video: false, audio: false});
-window.connections = {};
-peer.onconnection = function(connection) {
-	
-	log(null, 'connected: ' + connection.id);
-	connections[connection.id] = connection;
-	connected ++;
-	
-	//game.message = "Peer connected";
-	//update();
-	game.message = "It's " + game.a.name + "'s turn";
-	update();
-	//send(1);
-	
-	connection.ondisconnect = function() {
-		log(null,'disconnected: ' + connection.id);
-		delete connections[connection.id];
-		connected--;
-		game.message = "Game Over: Peer disconnected";
-		update();
-	};
-	connection.onerror = function(error) {
-		console.error(error);
-	};
-	
-	
-	connection.onmessage = function(label, msg) {
-		msg_data = JSON.parse(msg.data);
-		msg_data_data  = msg_data.msg;
-		
-		//if (msg_data.msg.init) {
-			game.turn = msg_data.msg.turn;
-			//game.tw = msg_data.msg.tw;
-			if (name == "b") {
-				game.a.name = msg_data.msg.a.name;	
-			} else if (name == "a") {
-				game.b.name = msg_data.msg.b.name;	
-			}
-		//} else {
-			game.move = msg_data.msg.move;
-		//}
-		
-		log(game.turn, JSON.stringify(msg_data_data));
-		if (game.move.type == "x")
-		{
-			x(game.move.x, game.move.y, true);
-		}
-		else if (game.move.type == "y")
-		{
-			y(game.move.x, game.move.y, true);
-		}
-	};
+function log(who, msg, time) {
+    if (time === null || time === undefined) {
+        var date = new Date();
+        time = date.toISOString();
+    }
+    var readable_date = new Date(time);
+    var readable_time = readable_date.toUTCString();
+
+    if (who === null) {
+        who = "Information";
+    }
+    var format_msg = ">> ";
+    format_msg += who;
+    format_msg += " &lt;";
+    format_msg += readable_time;
+    format_msg += "&gt; ";
+    format_msg += msg;
+    format_msg += "<br />";
+
+    msg_before = who;
+    $("#log").html($("#log").html() + format_msg);
+    console.log(who + " <" + time + "> " + msg);
+    scroll_bottom();
+}
+
+var connections = {};
+ws.onopen = function () {
+    connected++;
+    game.message = "It's " + game.a.name + "'s turn";
+    update();
+    //send(1);
 };
-peer.onerror = function(error) {
-	console.error(error);
+ws.onclose = function () {
+    connected--;
+    game.message = "Game Over: Peer disconnected";
+    update();
 };
 
+ws.onmessage = function (event) {
+    console.log(event);
+    var msg_data = JSON.parse(event.data);
+    if (!msg_data.msg) {
+        return false;
+    }
+    var msg_data_data  = msg_data.msg;
+    console.log(msg_data_data);
 
-if(hosting) { // host
-	console.log('hosting');
-	peer.listen({metadata:{name:'data-demo'}});
-	peer.onroute = function(route) {
-		var url = window.location.toString().split('?');
-		url[1] = url[1] || '';
-		var params = url[1].split('&');
-		params.push('webrtc-session=' + route);
-		url[1] = params.join('&');
-		
-		log(null, 'Send <a href="' + url.join('?') + '">this link</a> to other peer so that he/she can connect to you');
-		game.message = 'Send <a href="' + url.join('?') + '">this link</a> to other peer';
-		update();
-	}
-	name = "a";
+    //if (msg_data.msg.init) {
+    game.turn = msg_data.msg.turn;
+        //game.tw = msg_data.msg.tw;
+    if (name === "b") {
+        game.a.name = msg_data.msg.a.name;  
+    } else if (name === "a") {
+        game.b.name = msg_data.msg.b.name;  
+    }
+    //} else {
+    game.move = msg_data.msg.move;
+    //}
+
+    log(game.turn, JSON.stringify(msg_data_data));
+    if (game.move.type === "x") {
+        x(game.move.x, game.move.y, true);
+    } else if (game.move.type === "y") {
+        y(game.move.x, game.move.y, true);
+    }
+};
+
+if (hosting) { // host
+    name = "a";
 } else { // client
-	peer.connect(brokerSession);
-	name = "b";
+    name = "b";
 }
 
 // onload
-window.onload = function() {
-	log(null, "Peer-to-peer Dots and Boxes game with WebRTC; works in Firefox 24");
+window.onload = function () {
+    log(null, "Peer-to-peer Dots and Boxes game with WebRTC; works in Firefox 24");
 };
-
-// disconnect before unload
-window.onbeforeunload = function() {
-	var ids = Object.keys(connections);
-	ids.forEach(function(id) {
-		connections[id].close();
-	});
-	peer.close();
-};
-
-function scroll_bottom() {
-	// Scroll to bottom of #log
-	var log_area = document.getElementById("log");
-	log_area.scrollTop = log_area.scrollHeight;
-}
 
 function send(init) {
-    
-	
-	var date = new Date();
-	time = date.toISOString();
-	
-	if (init == null)
-	{
-		send_info = {
-			init: false,
-			turn: name,
-			a: {
-				name : game.a.name
-			},
-			b: {
-				name : game.b.name
-			},
-			move: game.move
-		};
-	}
-	else if (init == 1)
-	{
-		send_info = {
-			init: true,
-			turn: name,
-			a: {
-				name : game.a.name
-			},
-			b: {
-				name : game.b.name
-			},
-			tw: game.tw
-		};
-	}
-	
-    var ids = Object.keys(connections);
-	var send_msg = '{ "name": "' + name + '",'
-				 + '  "msg" : ' + JSON.stringify(send_info) + ','
-				 + '  "time" : "' + time + '" }';
-				 
-    log("Me", JSON.stringify(send_info), time);
-    ids.forEach(function(id) {
-		connections[id].send('reliable', send_msg);
-    });
+    var date = new Date();
+    var time = date.toISOString();
+    var send_info;
+    if (init === null || init === undefined) {
+        send_info = {
+            init: false,
+            turn: name,
+            a: {
+                name : game.a.name
+            },
+            b: {
+                name : game.b.name
+            },
+            move: game.move
+        };
+    } else if (init === 1) {
+        send_info = {
+            init: true,
+            turn: name,
+            a: {
+                name : game.a.name
+            },
+            b: {
+                name : game.b.name
+            },
+            tw: game.tw
+        };
+    }
 
+    var send_msg = '{ "name": "' + name + '",'
+                 + '  "msg" : ' + JSON.stringify(send_info) + ','
+                 + '  "time" : "' + time + '" }';
+
+    var data = JSON.stringify({type: "text", message: send_msg, author: name, time: new Date()});
+    log("Me", data, time);
+    ws.send(data);
 }
--- dot-star.js
+++ dot-star.js
@@ -1,3 +1,6 @@
+/*jslint sloppy: true, plusplus:true, browser: true, unparam:true, vars:true, continue:true */
+/*global $, jQuery, WebSocket, alert, prompt, connected, send */
+"use strict";
 /*
 
 TO DO!!!!
@@ -8,293 +11,293 @@ TO DO!!!!
 ## Prompt user's name
 
 */
-$(document).ready(function() {
-	initial();
-	promptu();
-	$("#toggle-log").click(function (){
-		$("#log").toggle();
-	});
-});
-game = {
-	turn: "a",
-	a: {
-		name : "A",
-		score : 0	
-	},
-	b: {
-		name : "B",
-		score : 0
-	},
-	message: "",
-	max_score: 0,
-	tw: 0,
-	move: {
-		type: "", // x or y
-		x: "",    // coord x
-		y: ""     // coord y
-	}
+var game = {
+    turn: "a",
+    a: {
+        name : "A",
+        score : 0
+    },
+    b: {
+        name : "B",
+        score : 0
+    },
+    message: "",
+    max_score: 0,
+    tw: 0,
+    move: {
+        type: "", // x or y
+        x: "",    // coord x
+        y: ""     // coord y
+    }
 };
-function initial() {
-	/* Initialization */
-	game = {
-		turn: "a",
-		a: {
-			name : "A",
-			score : 0	
-		},
-		b: {
-			name : "B",
-			score : 0
-		},
-		message: "",
-		max_score: 0,
-		tw: 0,
-		move: {
-			type: "", // x or y
-			x: "",    // coord x
-			y: ""     // coord y
-		}
-	};
-	game.message = "Loading";
-	
-	//game.message = "It's " + game.a.name + "'s turn";
-	update();
-	$("#log").hide();
-	$("#asc").removeClass("turn");
-	$("#bsc").removeClass("turn");
-	$("#asc").addClass("turn");
-}
 function update() {
-	console.log(game);
-	$("#aturn").val(game.turn);
-	$("#message").html(game.message);
-	$("#asc").html(game.a.score);
-	$("#bsc").html(game.b.score);
-	$("#aname").html(game.a.name);
-	$("#bname").html(game.b.name);
-		
+    console.log(game);
+    $("#aturn").val(game.turn);
+    $("#message").html(game.message);
+    $("#asc").html(game.a.score);
+    $("#bsc").html(game.b.score);
+    $("#aname").html(game.a.name);
+    $("#bname").html(game.b.name);
 }
-function reloadpage() {
-	initial();
-	promptu();
-}
-function x(xa,ya, prev_send) {
-	if (connected == 0)
-	{
-		alert("Peer not yet connected");
-		return false;		
-	}
-	else if ((game.turn != name) && (prev_send == null))
-	{
-		alert("It's your peer's turn!");
-		return false;	
-	}
-	var txy="#linex"+xa+""+ya;
-	if ($(txy).hasClass("line")) {
-		game.message = "Please choose another line";
-	} else {
-		$(txy).addClass("line");
-		$(txy).removeClass("pline");
-		checkboxx(xa,ya);
-	}
-	update();
-	game.move.type = "x";
-	game.move.x = xa;
-	game.move.y = ya;
-	if (prev_send == null) {
-		send();
-	}
-}
-function y(xb,yb, prev_send) {
-	if (connected == 0)
-	{
-		alert("Peer not yet connected");
-		return false;		
-	}
-	else if ((game.turn != name) && (prev_send == null))
-	{
-		alert("It's your peer's turn!");
-		return false;	
-	}
-	var sxy="#liney"+xb+""+yb;
-	if ($(sxy).hasClass("line")) {
-		game.message = "Please choose another line";
-	} else {
-		$(sxy).addClass("line");
-		$(sxy).removeClass("pline");
-		checkboxy(xb,yb);
-	}
-	update();
-	game.move.type = "y";
-	game.move.x = xb;
-	game.move.y = yb;
-	if (prev_send == null) {
-		send();
-	}
-}
-function checkboxx(xc,yc) {
-	var cxy="#linex"+xc+""+(yc-1);
-	var dxy="#liney"+xc+""+(yc-1);
-	var exy="#liney"+(xc+1)+""+(yc-1);
-	var fxy="#linex"+xc+""+(yc+1);
-	var gxy="#liney"+(xc+1)+""+yc;
-	var hxy="#liney"+xc+""+yc;
-	if ((cbox(cxy) && cbox(dxy) && cbox(exy)) || (cbox(fxy) && cbox(gxy) && cbox(hxy))) {
-		if (cbox(cxy) && cbox(dxy) && cbox(exy)) {
-			var ixy="#box"+xc+(yc-1);
-			addColor(ixy);
-		}
-		if (cbox(fxy) && cbox(gxy) && cbox(hxy)) {
-			var ixy="#box"+xc+yc;
-			addColor(ixy);
-		}
-	} else {
-		invertTurn();
-	}
-}
-function checkboxy(xc,yc) {
-	var c1xy="#liney"+(xc-1)+""+yc;
-	var d1xy="#linex"+(xc-1)+""+yc;
-	var e1xy="#linex"+(xc-1)+""+(yc+1);
-	var f1xy="#liney"+(xc+1)+""+yc;
-	var g1xy="#linex"+xc+""+yc;
-	var h1xy="#linex"+xc+""+(yc+1);
-	if ((cbox(c1xy) && cbox(d1xy) && cbox(e1xy)) || (cbox(f1xy) && cbox(g1xy) && cbox(h1xy))) {
-		if (cbox(c1xy) && cbox(d1xy) && cbox(e1xy)) {
-			var i1xy="#box"+(xc-1)+yc;
-			addColor(i1xy);
-		}
-		if (cbox(f1xy) && cbox(g1xy) && cbox(h1xy)) {
-			var i1xy="#box"+xc+yc;
-			addColor(i1xy);
-		}
-	} else {
-		invertTurn();
-	}
-}
-function cbox(xyz) {
-	if ($(xyz).hasClass("line")) {
-		return true;
-	} else {
-		return false;
-	}
-}
-function addColor(xyz) {
-	var anam = game.a.name;
-	var bnam = game.b.name;
-	var turn = game.turn;
-	//var score;
-	if (turn == "a") {
-		$(xyz).addClass("abox");
-		game.a.score += 1;
-		game.message = "Good job! It's " + anam + "'s turn again";
-	} else if (turn=="b") {
-		$(xyz).addClass("bbox");
-		game.b.score += 1;
-		game.message = "Good job! It's " + bnam + "'s turn again";
-	}
-	//$('#message').html(game.message);
-	update();
-	
-	var ascore = game.a.score;
-	var bscore = game.b.score;
-	var totalscore = ascore-(-bscore);
-	var maxscore = game.max_score;
-	if (totalscore >= maxscore) {
-		var temp = "Game Over: ";
-		if (ascore > bscore) {
-			temp += anam + " win! ";
-		} else if (ascore < bscore) {
-			temp += bnam + " win! ";
-		} else { //ascore == bscore
-			temp += "It's a tie! ";
-		}
-		game.message = temp;
-		update();
-		alert(temp);
-	}
-}
-function invertTurn() {
-	var anam = game.a.name;
-	var bnam = game.b.name;
-	var turn = game.turn;
-	if (turn=="a") {
-		$("#asc").removeClass("turn");
-		$("#bsc").addClass("turn");
-		turn = "b";
-		game.message = "It's " + bnam + "'s turn";
-	} else if (turn=="b") {
-		$("#bsc").removeClass("turn");
-		$("#asc").addClass("turn");
-		turn = "a";
-		game.message = "It's " + anam + "'s turn";
-	}
-	game.turn = turn;
-	update();
-}
-function promptu() {
-	var tw = 3;
-	
-	var anam = 'A';
-	if (name == "a") anam = prompt('Input your name','A');
-	game.a.name = anam;
-	
-	if (name == "a")
-	{
-		tw = 3;
-		if ((isNaN(tw))||(tw>10)||(tw<1)) {
-			while ((isNaN(tw))||(tw>10)||(tw<1)) {
-				tw = prompt('Input table width \n(1 ≤ width ≤ 10)', 3);
-			}
-		}
-	}
-	game.tw = tw;
-	
-	var bnam = 'B';//prompt('Input player 2 name','B');
-	if (name == "b") bnam = prompt('Input your name','B');
-	game.b.name = bnam;
-	
-	//send(1);
-	
-	draw_table(game.tw);
-	
-	game.max_score = tw*tw;
-	var sb="Scoreboard<br />"
-	$('#sboard').html(sb + "("+tw+"×"+tw+")");
-	
-	$('#aname').html(anam);
-	$('#bname').html(bnam);
-	update();
+function initial() {
+    /* Initialization */
+    game = {
+        turn: "a",
+        a: {
+            name : "A",
+            score : 0
+        },
+        b: {
+            name : "B",
+            score : 0
+        },
+        message: "",
+        max_score: 0,
+        tw: 0,
+        move: {
+            type: "", // x or y
+            x: "",    // coord x
+            y: ""     // coord y
+        }
+    };
+    game.message = "Loading";
+
+    //game.message = "It's " + game.a.name + "'s turn";
+    update();
+    $("#log").hide();
+    $("#asc").removeClass("turn");
+    $("#bsc").removeClass("turn");
+    $("#asc").addClass("turn");
 }
 function draw_table(tw) {
-	var maint="";
-	var ri = 0;
-	var rj = 0;
-	for (var i=1;i<=(tw*2+1);i++) {
-		maint=maint + "<tr>";
-		rj=0;
-		if (i%2==1) {
-			ri=ri+1;
-		}
-		for (var j=1;j<=(tw*2+1);j++) {
-			
-			if (i%2==1) {
-				if (j%2==1) {
-					maint=maint+"<td class='sdot game_elem'>&nbsp;</td></td>";
-				} else {
-					rj=rj+1;
-					maint=maint+"<td class='pline hline game_elem' id='linex"+rj+ri+"' onclick='x("+rj+","+ri+")'>&nbsp;</td>";
-				}
-			} else {
-				if (j%2==1) {
-					rj=rj+1;
-					maint=maint+"<td class='pline vline game_elem' id='liney"+rj+ri+"' onclick='y("+rj+","+ri+")'>&nbsp;</td>";
-				} else {
-					maint=maint+"<td class='box game_elem' id='box"+rj+ri+"'>&nbsp;</td>";
-				}
-			}
-		}
-		maint=maint+"</tr>";
-	}
-	$("#maintable").html(maint);
+    var maint = "";
+    var ri = 0;
+    var rj = 0;
+    var i, j;
+    for (i = 1; i <= (tw * 2 + 1); i++) {
+        maint = maint + "<tr>";
+        rj = 0;
+        if (i % 2 === 1) {
+            ri = ri + 1;
+        }
+        for (j = 1; j <= (tw * 2 + 1); j++) {
+            if (i % 2 === 1) {
+                if (j % 2 === 1) {
+                    maint = maint + "<td class='sdot game_elem'>&nbsp;</td></td>";
+                } else {
+                    rj = rj + 1;
+                    maint = maint + "<td class='pline hline game_elem' id='linex" + rj + ri + "' onclick='x(" + rj + "," + ri + ")'>&nbsp;</td>";
+                }
+            } else {
+                if (j % 2 === 1) {
+                    rj = rj + 1;
+                    maint = maint + "<td class='pline vline game_elem' id='liney" + rj + ri + "' onclick='y(" + rj + "," + ri + ")'>&nbsp;</td>";
+                } else {
+                    maint = maint + "<td class='box game_elem' id='box" + rj + ri + "'>&nbsp;</td>";
+                }
+            }
+        }
+        maint = maint + "</tr>";
+    }
+    $("#maintable").html(maint);
+}
+function promptu() {
+    var tw = 3;
+
+    var anam = 'A';
+    if (name === "a") {
+        anam = prompt('Input your name', 'A');
+    }
+    game.a.name = anam;
+
+    if (name === "a") {
+        tw = 3;
+        if ((isNaN(tw)) || (tw > 10) || (tw < 1)) {
+            while ((isNaN(tw)) || (tw > 10) || (tw < 1)) {
+                tw = prompt('Input table width \n(1 ≤ width ≤ 10)', 3);
+            }
+        }
+    }
+    game.tw = tw;
+
+    var bnam = 'B';//prompt('Input player 2 name','B');
+    if (name === "b") {
+        bnam = prompt('Input your name', 'B');
+    }
+    game.b.name = bnam;
+
+    //send(1);
+
+    draw_table(game.tw);
+
+    game.max_score = tw * tw;
+    var sb = "Scoreboard<br />";
+    $('#sboard').html(sb + "(" + tw + "×" + tw + ")");
+
+    $('#aname').html(anam);
+    $('#bname').html(bnam);
+    update();
+}
+function reloadpage() {
+    initial();
+    promptu();
+}
+$(document).ready(function () {
+    initial();
+    promptu();
+    $("#toggle-log").click(function () {
+        $("#log").toggle();
+    });
+});
+
+function addColor(xyz) {
+    var anam = game.a.name;
+    var bnam = game.b.name;
+    var turn = game.turn;
+    //var score;
+    if (turn === "a") {
+        $(xyz).addClass("abox");
+        game.a.score += 1;
+        game.message = "Good job! It's " + anam + "'s turn again";
+    } else if (turn === "b") {
+        $(xyz).addClass("bbox");
+        game.b.score += 1;
+        game.message = "Good job! It's " + bnam + "'s turn again";
+    }
+    //$('#message').html(game.message);
+    update();
+
+    var ascore = game.a.score;
+    var bscore = game.b.score;
+    var totalscore = ascore - (-bscore);
+    var maxscore = game.max_score;
+    if (totalscore >= maxscore) {
+        var temp = "Game Over: ";
+        if (ascore > bscore) {
+            temp += anam + " win! ";
+        } else if (ascore < bscore) {
+            temp += bnam + " win! ";
+        } else { //ascore == bscore
+            temp += "It's a tie! ";
+        }
+        game.message = temp;
+        update();
+        alert(temp);
+    }
+}
+function invertTurn() {
+    var anam = game.a.name;
+    var bnam = game.b.name;
+    var turn = game.turn;
+    if (turn === "a") {
+        $("#asc").removeClass("turn");
+        $("#bsc").addClass("turn");
+        turn = "b";
+        game.message = "It's " + bnam + "'s turn";
+    } else if (turn === "b") {
+        $("#bsc").removeClass("turn");
+        $("#asc").addClass("turn");
+        turn = "a";
+        game.message = "It's " + anam + "'s turn";
+    }
+    game.turn = turn;
+    update();
+}
+function cbox(xyz) {
+    if ($(xyz).hasClass("line")) {
+        return true;
+    }
+    return false;
+}
+function checkboxx(xc, yc) {
+    var cxy = "#linex" + xc         + (yc - 1);
+    var dxy = "#liney" + xc         + (yc - 1);
+    var exy = "#liney" + (xc + 1)   + (yc - 1);
+    var fxy = "#linex" + xc         + (yc + 1);
+    var gxy = "#liney" + (xc + 1)   + yc;
+    var hxy = "#liney" + xc         + yc;
+    var ixy;
+    if ((cbox(cxy) && cbox(dxy) && cbox(exy)) || (cbox(fxy) && cbox(gxy) && cbox(hxy))) {
+        if (cbox(cxy) && cbox(dxy) && cbox(exy)) {
+            ixy = "#box" + xc + (yc - 1);
+            addColor(ixy);
+        }
+        if (cbox(fxy) && cbox(gxy) && cbox(hxy)) {
+            ixy = "#box" + xc + yc;
+            addColor(ixy);
+        }
+    } else {
+        invertTurn();
+    }
+}
+function checkboxy(xc, yc) {
+    var c1xy = "#liney" + (xc - 1)  + yc;
+    var d1xy = "#linex" + (xc - 1)  + yc;
+    var e1xy = "#linex" + (xc - 1)  + (yc + 1);
+    var f1xy = "#liney" + (xc + 1)  + yc;
+    var g1xy = "#linex" + xc        + yc;
+    var h1xy = "#linex" + xc        + (yc + 1);
+    var i1xy;
+    if ((cbox(c1xy) && cbox(d1xy) && cbox(e1xy)) || (cbox(f1xy) && cbox(g1xy) && cbox(h1xy))) {
+        if (cbox(c1xy) && cbox(d1xy) && cbox(e1xy)) {
+            i1xy = "#box" + (xc - 1) + yc;
+            addColor(i1xy);
+        }
+        if (cbox(f1xy) && cbox(g1xy) && cbox(h1xy)) {
+            i1xy = "#box" + xc + yc;
+            addColor(i1xy);
+        }
+    } else {
+        invertTurn();
+    }
+}
+function x(xa, ya, prev_send) {
+    if (connected === 0) {
+        alert("Peer not yet connected");
+        return false;
+    }
+    if ((game.turn !== name) && (prev_send === null || prev_send === undefined)) {
+        alert("It's your peer's turn!");
+        return false;
+    }
+    var txy = "#linex" + xa + ya;
+    if ($(txy).hasClass("line")) {
+        game.message = "Please choose another line";
+    } else {
+        $(txy).addClass("line");
+        $(txy).removeClass("pline");
+        checkboxx(xa, ya);
+    }
+    update();
+    game.move.type = "x";
+    game.move.x = xa;
+    game.move.y = ya;
+    if (prev_send === null || prev_send === undefined) {
+        send();
+    }
+}
+function y(xb, yb, prev_send) {
+    if (connected === 0) {
+        alert("Peer not yet connected");
+        return false;
+    }
+    if ((game.turn !== name) && (prev_send === null)) {
+        alert("It's your peer's turn!");
+        return false;
+    }
+    var sxy = "#liney" + xb + yb;
+    if ($(sxy).hasClass("line")) {
+        game.message = "Please choose another line";
+    } else {
+        $(sxy).addClass("line");
+        $(sxy).removeClass("pline");
+        checkboxy(xb, yb);
+    }
+    update();
+    game.move.type = "y";
+    game.move.x = xb;
+    game.move.y = yb;
+    if (prev_send === null || prev_send === undefined) {
+        send();
+    }
 }
